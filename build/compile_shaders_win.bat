6dd3aeb960d8ea2fd6d33317df07960490c5fa3a
snake5 <snake5creator@gmail.com>
2015-12-26 00:21:54 +0200

    simplified shader compilation

--- build/compile_shaders_win.bat
+++ build/compile_shaders_win.bat
@@ -1,6 +1,5 @@
-fxc /Zi /E"main" /Od /Fo"ps_texture.cso" /T ps_4_0_level_9_1 /nologo ../ouzel/win/ps_texture.hlsl
-fxc /Zi /E"main" /Od /Fo"vs_common.cso" /T vs_4_0_level_9_1 /nologo ../ouzel/win/vs_common.hlsl
+fxc /Zi /E"main" /Od /Fh"ps_texture.csoinl" /Vn"D3D11_SHADER_PS_TEXTURE" /T ps_4_0_level_9_1 /nologo ../ouzel/win/ps_texture.hlsl
+fxc /Zi /E"main" /Od /Fh"vs_common.csoinl" /Vn"D3D11_SHADER_VS_COMMON" /T vs_4_0_level_9_1 /nologo ../ouzel/win/vs_common.hlsl
 echo. > D3D11Shaders.inl
-bin2h D3D11_SHADER_PS_TEXTURE < ps_texture.cso >> D3D11Shaders.inl
-bin2h D3D11_SHADER_VS_COMMON < vs_common.cso >> D3D11Shaders.inl
-powershell -Command "(gc D3D11Shaders.inl) -replace 'char', 'uint8_t' | Out-File D3D11Shaders.inl"
+type ps_texture.csoinl >> D3D11Shaders.inl
+type vs_common.csoinl >> D3D11Shaders.inl
